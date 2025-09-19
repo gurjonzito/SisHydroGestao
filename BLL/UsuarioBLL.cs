a4d420a69048effbd73c0c3b0cc5fb0b9f0d5ffc
Alan M. <alan.elias@aluno.unifafibe.edu.br>
2025-09-19 16:56:06 -0300

    Updates

---
+++ BLL/UsuarioBLL.cs
@@ -0,0 +1,110 @@
+﻿using DAL;
+using model;
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+
+namespace BLL
+{
+    public class UsuarioBLL
+    {
+        private UsuarioDAL usuarioDAL = new UsuarioDAL();
+
+        private string ValidarUsuario(Usuario usuario)
+        {
+            if (string.IsNullOrWhiteSpace(usuario.Nome))
+                return "Informe o nome do usuário.";
+
+            if (string.IsNullOrWhiteSpace(usuario.Email))
+                return "Informe o e-mail do usuário.";
+
+            if (string.IsNullOrWhiteSpace(usuario.Senha))
+                return "Informe a senha do usuário.";
+
+            if (usuario.Login.Contains(" "))
+                return "Usuário não pode conter espaços em branco.";
+
+            if (usuario.Email.Contains(" "))
+                return "E-mail não pode conter espaços em branco.";
+
+            if (usuario.Senha.Contains(" "))
+                return "Senha não pode conter espaços em branco.";
+
+            if (IsValidEmail(usuario.email))
+                return "E-mail inválido.";
+
+            return "OK";
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            try
+            {
+                MailAddress mailAddress = new MailAddress(email);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        public bool VerificarCredenciais(string usuario, string senha)
+        {
+            Usuario user = usuarioDAL.ObterUsuarioPorLogin(usuario);
+
+            if (user != null)
+            {
+                string senhaCriptografadaDoBanco = user.Senha;
+
+                if (senhaCriptografadaDoBanco != null)
+                {
+                    // Verificar a senha
+                    if (BCrypt.Net.BCrypt.Verify(senha, senhaCriptografadaDoBanco))
+                    {
+                        return true; // Credenciais válidas
+                    }
+                }
+            }
+
+            return false; // Credenciais inválidas
+        }
+
+        public string CadastrarUsuario(Usuario usuario)
+        {
+            string validacao = ValidarUsuario(usuario);
+            if (validacao != "OK") return validacao;
+
+            try
+            {
+                usuarioDAL.InserirUsuario(usuario);
+                return "Sucesso";
+            }
+            catch (Exception ex)
+            {
+                return $"Erro ao cadastrar: {ex.Message}";
+            }
+        }
+
+        public string EditarUsuario(Usuario usuario)
+        {
+            string validacao = ValidarUsuario(usuario);
+            if (validacao != "OK") return validacao;
+
+            try
+            {
+                usuarioDAL.EditarUsuario(usuario);
+                return "Sucesso";
+            }
+            catch (Exception ex)
+            {
+                return $"Erro ao cadastrar: {ex.Message}";
+            }
+        }
+
+        public List<Usuario> GetUsuarios()
+        {
+            return usuarioDAL.GetUsuarios();
+        }
+    }
+}
---
+++ DAL/UsuarioDAL.cs
@@ -0,0 +1,76 @@
+﻿using Dapper;
+using model;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        ConexaoDB mConn = new ConexaoDB();
+        string sql;
+        MySqlCommand cmd;
+
+        public string HashPassword(string password)
+        {
+            string salt = BCrypt.Net.BCrypt.GenerateSalt();
+            return BCrypt.Net.BCrypt.HashPassword(password, salt);
+        }
+
+        public string ObterSenhaCriptografada(string usuario)
+        {
+            using (IDbConnection dbConnection = mConn.AbrirConexao())
+            {
+                string query = "SELECT senha FROM usuario WHERE login = @Login";
+                return dbConnection.ExecuteScalar<string>(query, new { usuario });
+            }
+        }
+
+        public void InserirUsuario(Usuario usuario)
+        {
+            string senhaNaoCriptografada = usuario.Senha;
+            string senhaCriptografada = HashPassword(senhaNaoCriptografada);
+            usuario.Senha = senhaCriptografada;
+
+            sql = "INSERT INTO usuario (nome, email, login, senha) VALUES (@Nome, @Email, @Login, @Senha)";
+            cmd = new MySqlCommand(sql, mConn.AbrirConexao());
+
+            cmd.Parameters.AddWithValue("@Nome", usuario.Nome);
+            cmd.Parameters.AddWithValue("@Email", usuario.Email);
+            cmd.Parameters.AddWithValue("@Login", usuario.Login);
+            cmd.Parameters.AddWithValue("@Senha", usuario.Senha);
+
+            cmd.ExecuteNonQuery();
+
+            mConn.FecharConexao();
+        }
+
+        public void EditarUsuario(Usuario usuario)
+        {
+            using (IDbConnection dbConnection = mConn.AbrirConexao())
+            {
+                string query = "UPDATE usuario SET nome = @Nome, email = @Email, login = @Login, senha = @Senha WHERE user_id = @user_id";
+                dbConnection.Execute(query, usuario);
+            }
+        }
+
+        public List<Usuario> GetUsuarios()
+        {
+            using (IDbConnection dbConnection = mConn.AbrirConexao())
+            {
+                string query = "SELECT * FROM usuario";
+                return dbConnection.Query<Usuario>(query).AsList();
+            }
+        }
+
+        public Usuario ObterUsuarioPorLogin(string usuario)
+        {
+            using (IDbConnection dbConnection = mConn.AbrirConexao())
+            {
+                string query = "SELECT * FROM usuario WHERE login = @Login";
+                return dbConnection.QueryFirstOrDefault<Usuario>(query, new { usuario });
+            }
+        }
+    }
+}
